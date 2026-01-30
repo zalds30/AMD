8d632564e1748f0acba7ce4855b957e59f3aeec0
Zaldy Delos Reyes <zalddfox26@gmail.com>
2026-01-30 22:56:51 +0800

    update

--- AMD/Program.cs
+++ AMD/Program.cs
@@ -5,22 +5,13 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
+// Remove HSTS and HTTPS redirection for Render
+// app.UseHttpsRedirection(); // COMMENT OR REMOVE
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
-app.Run();
+// Render PORT setup
+app.Run($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "5000"}");
